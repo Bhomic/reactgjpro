ebe746552bf4834cf2695ee63f3f89b807b5a2c2
bhomic <bhomickaushik174@gmail.com>
2021-11-10 17:32:00 +0530

    Fetch implemetation : fetch from server cross-fetch implementation : instead of fetching from assets fetch from server running at localhost:3001

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -7,6 +7,8 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
 
 import { Loading } from './LoadingComponent';
 
+import { baseUrl } from '../shared/baseUrl';
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -166,7 +168,7 @@ function RenderDish(props){
         return(
             <>
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>
                         {dish.name}
